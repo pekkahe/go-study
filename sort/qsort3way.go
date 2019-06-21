18d0d9a8ab589e02307672011fb8a6a5bfe6ab3a
pekkahe <pekka.hellsten@gmail.com>
2019-06-21 14:17:35 +0300

    Tabs to spaces

--- sort/qsort3way.go
+++ sort/qsort3way.go
@@ -1,88 +1,88 @@
 package sort
 
 func QuickSort3Way(arr []int) {
-	quickSortImpl3Way(arr, 0, len(arr)-1)
+    quickSortImpl3Way(arr, 0, len(arr)-1)
 }
 
 func quickSortImpl3Way(arr []int, lo, hi int) {
-	if lo < hi {
-		pivot := partition3way(arr, lo, hi, hi)
+    if lo < hi {
+        pivot := partition3way(arr, lo, hi, hi)
 
-		//padding := lo * 2
-		// fmt.Printf("Current:     %v\n", arr)
-		// fmt.Printf("Partition:   %s%v pivot: [%d]\n",
-		// 	strings.Repeat(" ", padding),
-		// 	arr[lo:hi+1],
-		// 	pivot)
-		// fmt.Printf("Left/Right:  %s%v %v\n",
-		// 	strings.Repeat(" ", padding),
-		// 	arr[lo:pivot],
-		// 	arr[pivot+1:hi+1])
+        //padding := lo * 2
+        // fmt.Printf("Current:     %v\n", arr)
+        // fmt.Printf("Partition:   %s%v pivot: [%d]\n",
+        // 	strings.Repeat(" ", padding),
+        // 	arr[lo:hi+1],
+        // 	pivot)
+        // fmt.Printf("Left/Right:  %s%v %v\n",
+        // 	strings.Repeat(" ", padding),
+        // 	arr[lo:pivot],
+        // 	arr[pivot+1:hi+1])
 
-		// fmt.Printf("Left:        %s%v ->\n",
-		// 	strings.Repeat(" ", padding),
-		// 	arr[lo:pivot])
+        // fmt.Printf("Left:        %s%v ->\n",
+        // 	strings.Repeat(" ", padding),
+        // 	arr[lo:pivot])
 
-		quickSortImpl3Way(arr, lo, pivot-1)
+        quickSortImpl3Way(arr, lo, pivot-1)
 
-		// fmt.Printf("Right:       %s%v ->\n",
-		// 	strings.Repeat(" ", (pivot+1)*2),
-		// 	arr[pivot+1:hi+1])
+        // fmt.Printf("Right:       %s%v ->\n",
+        // 	strings.Repeat(" ", (pivot+1)*2),
+        // 	arr[pivot+1:hi+1])
 
-		quickSortImpl3Way(arr, pivot+1, hi)
-	}
+        quickSortImpl3Way(arr, pivot+1, hi)
+    }
 }
 
 func partition3way(arr []int, lo, hi, p int) int {
-	// padding := lo * 2
-	// t1, t2 := lo, hi
-	// fmt.Printf("  Partition: %s%v indices: [%d-%d] pivot: [%d]=%d\n",
-	// 	strings.Repeat(" ", padding),
-	// 	arr[lo:hi+1],
-	// 	lo,
-	// 	hi,
-	// 	p,
-	// 	arr[p])
+    // padding := lo * 2
+    // t1, t2 := lo, hi
+    // fmt.Printf("  Partition: %s%v indices: [%d-%d] pivot: [%d]=%d\n",
+    // 	strings.Repeat(" ", padding),
+    // 	arr[lo:hi+1],
+    // 	lo,
+    // 	hi,
+    // 	p,
+    // 	arr[p])
 
-	pivot := arr[p]
-	for mid := lo; mid <= hi; {
-
-		// marks := make([]string, len(arr))
-		// for i := range marks {
-		// 	marks[i] = " "
-		// }
-		// marks[lo] = "L"
-		// marks[mid] = "M"
-		// marks[hi] = "H"
-		// fmt.Printf("             %s%v\n",
-		// 	strings.Repeat(" ", padding),
-		// 	arr[t1:t2+1])
-		// fmt.Printf("              %s%s",
-		// 	strings.Repeat(" ", padding),
-		// 	strings.Join(marks[t1:t2+1], " "))
-
-		switch {
-		case arr[mid] < pivot:
-			arr[mid], arr[lo] = arr[lo], arr[mid]
-			mid++
-			lo++
-
-			//fmt.Printf("    M < %d  -> swap(M,L), M++, L++", pivot)
-		case arr[mid] == pivot:
-			mid++
-
-			//fmt.Printf("    M == %d -> M++", pivot)
-		default: // arr[mid] > pivot
-			arr[mid], arr[hi] = arr[hi], arr[mid]
-			hi--
-
-			///fmt.Printf("    M > %d  -> swap(M,H), H--", pivot)
-		}
-		//fmt.Print("\n")
-	}
-	// fmt.Printf("  Done:      %s%v\n",
-	// 	strings.Repeat(" ", padding),
-	// 	arr[t1:t2+1])
-
-	return lo
+    pivot := arr[p]
+    for mid := lo; mid <= hi; {
+    
+       // marks := make([]string, len(arr))
+       // for i := range marks {
+       // 	marks[i] = " "
+       // }
+       // marks[lo] = "L"
+       // marks[mid] = "M"
+       // marks[hi] = "H"
+       // fmt.Printf("             %s%v\n",
+       // 	strings.Repeat(" ", padding),
+       // 	arr[t1:t2+1])
+       // fmt.Printf("              %s%s",
+       // 	strings.Repeat(" ", padding),
+       // 	strings.Join(marks[t1:t2+1], " "))
+        
+       switch {
+       case arr[mid] < pivot:
+           arr[mid], arr[lo] = arr[lo], arr[mid]
+           mid++
+           lo++
+        
+           //fmt.Printf("    M < %d  -> swap(M,L), M++, L++", pivot)
+       case arr[mid] == pivot:
+           mid++
+        
+           //fmt.Printf("    M == %d -> M++", pivot)
+       default: // arr[mid] > pivot
+           arr[mid], arr[hi] = arr[hi], arr[mid]
+           hi--
+        
+           ///fmt.Printf("    M > %d  -> swap(M,H), H--", pivot)
+       }
+       //fmt.Print("\n")
+    }
+    // fmt.Printf("  Done:      %s%v\n",
+    // 	strings.Repeat(" ", padding),
+    // 	arr[t1:t2+1])
+    
+    return lo
 }
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,117 +1,117 @@
 package sort_test
 
 import (
-	"fmt"
-	"math/rand"
-	"os"
-	"reflect"
-	"runtime"
-	"strings"
-	"sort"
-	"testing"
-	"time"
-	pesort "github.com/pekkahe/go-study/sort"
+    "fmt"
+    "math/rand"
+    "os"
+    "reflect"
+    "runtime"
+    "strings"
+    "sort"
+    "testing"
+    "time"
+    pesort "github.com/pekkahe/go-study/sort"
 )
 
 var unsorted []int
 var sorted []int
 
 func TestMain(m *testing.M) {
-	unsorted = generateSingleDigitNumbers(30000)
-	sorted = append([]int(nil), unsorted...)
-
-	start := time.Now()
-	sort.Ints(sorted)
-	elapsed := time.Since(start)
-
-	fmt.Printf("Reference: %s\n", elapsed)
-
+    unsorted = generateSingleDigitNumbers(30000)
+    sorted = append([]int(nil), unsorted...)
+    
+    start := time.Now()
+    sort.Ints(sorted)
+    elapsed := time.Since(start)
+    
+    fmt.Printf("Reference: %s\n", elapsed)
+    
     os.Exit(m.Run())
 }
 
 func generateSingleDigitNumbers(n int) []int {
-	v := make([]int, n)
+    v := make([]int, n)
 
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < len(v); i++ {
-		v[i] = rand.Int() % 10
-	}
+    rand.Seed(time.Now().UnixNano())
+    for i := 0; i < len(v); i++ {
+       v[i] = rand.Int() % 10
+    }
 
-	const printMaxN int = 20
-	fmt.Printf("Numbers (n=%d): ", len(v))
-	if len(v) > printMaxN {
-		fmt.Printf("%v ...\n", v[:printMaxN])
-	} else {
-		fmt.Printf("%v\n", v)
-	}
+    const printMaxN int = 20
+    fmt.Printf("Numbers (n=%d): ", len(v))
+    if len(v) > printMaxN {
+       fmt.Printf("%v ...\n", v[:printMaxN])
+    } else {
+       fmt.Printf("%v\n", v)
+    }
 
-	return v
+    return v
 }
 
 func TestQuickSort(t *testing.T) {
-	runTest(t, pesort.QuickSort)
+    runTest(t, pesort.QuickSort)
 }
 
 func TestQuickSort3Way(t *testing.T) {
-	runTest(t, pesort.QuickSort3Way)
+    runTest(t, pesort.QuickSort3Way)
 }
 
 func TestInsertionSort(t *testing.T) {
-	runTest(t, pesort.InsertionSort)
+    runTest(t, pesort.InsertionSort)
 }
 
 func TestSelectionSort(t *testing.T) {
-	runTest(t, pesort.SelectionSort)
+    runTest(t, pesort.SelectionSort)
 }
 
 func TestBubbleSort(t *testing.T) {
-	runTest(t, pesort.BubbleSort)
+    runTest(t, pesort.BubbleSort)
 }
 
 func runTest(t *testing.T, sortF func([]int)) {
-	v := append([]int(nil), unsorted...)
+    v := append([]int(nil), unsorted...)
 
-	start := time.Now()
-	sortF(v)
-	elapsed := time.Since(start)
+    start := time.Now()
+    sortF(v)
+    elapsed := time.Since(start)
 
-	fmt.Printf("%s: %s\n", getFunctionName(sortF), elapsed)
+    fmt.Printf("%s: %s\n", getFunctionName(sortF), elapsed)
 
-	assertEqual(t, sorted, v)
+    assertEqual(t, sorted, v)
 }
 
 func getFunctionName(i interface{}) string {
-	fullName := runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
-	parts := strings.Split(fullName, ".")
-	return parts[len(parts)-1]
+    fullName := runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+    parts := strings.Split(fullName, ".")
+    return parts[len(parts)-1]
 }
 
 func assertEqual(t *testing.T, expected, actual []int) {
-	if len(actual) != len(expected) {
-		t.Errorf("len(actual) != len(expected) => %d != %d\n",
-			len(actual),
-			len(expected))
-		return
-	}
+    if len(actual) != len(expected) {
+       t.Errorf("len(actual) != len(expected) => %d != %d\n",
+           len(actual),
+           len(expected))
+       return
+    }
 
-	var errors []int
-	const printErrors = 3
+    var errors []int
+    const printErrors = 3
 
-	for i := 0; i < len(actual); i++ {
-		if actual[i] != expected[i] {
-			errors = append(errors, i)
-			if len(errors) > printErrors {
-				break
-			}
-		}
-	}
+    for i := 0; i < len(actual); i++ {
+       if actual[i] != expected[i] {
+           errors = append(errors, i)
+           if len(errors) > printErrors {
+               break
+           }
+       }
+    }
 
-	for i := 0; i < len(errors) && i < printErrors; i++ {
-		idx := errors[i]
-		t.Errorf("actual[%d] != expected[%d] => %d != %d\n", idx, idx, actual[idx], expected[idx])
-	}
+    for i := 0; i < len(errors) && i < printErrors; i++ {
+       idx := errors[i]
+       t.Errorf("actual[%d] != expected[%d] => %d != %d\n", idx, idx, actual[idx], expected[idx])
+    }
 
-	if len(errors) > printErrors {
-		t.Errorf("...\n")
-	}
+    if len(errors) > printErrors {
+       t.Errorf("...\n")
+    }
 }
