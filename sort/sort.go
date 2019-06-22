a10d8c38aecb24798e8a68e453ae65ef3308cefa
pekkahe <pekka.hellsten@gmail.com>
2019-06-22 12:52:53 +0300

    Revert back to tabs, Go formatting tool forces this.

--- sort/sort.go
+++ sort/sort.go
@@ -1,37 +1,37 @@
 package sort
 
 func InsertionSort(arr []int) {
-    for i := 1; i < len(arr); i++ {
-       key := arr[i]
-       j := i - 1
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
 
-       for j >= 0 && arr[j] > key {
-           arr[j+1] = arr[j]
-           j--
-       }
-       
-       arr[j+1] = key
-    }
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+
+		arr[j+1] = key
+	}
 }
 
 func SelectionSort(arr []int) {
-    for i := 0; i < len(arr); i++ {
-       min := i
-       for j := i + 1; j < len(arr); j++ {
-           if arr[j] < arr[min] {
-               min = j
-           }
-       }
-       arr[i], arr[min] = arr[min], arr[i]
-    }
+	for i := 0; i < len(arr); i++ {
+		min := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+		arr[i], arr[min] = arr[min], arr[i]
+	}
 }
 
 func BubbleSort(arr []int) {
-    for end := len(arr); end > 0; end-- {
-        for i := 1; i < end; i++ {
-            if arr[i-1] > arr[i] {
-                arr[i], arr[i-1] = arr[i-1], arr[i]
-            }
-        }
-    }
+	for end := len(arr); end > 0; end-- {
+		for i := 1; i < end; i++ {
+			if arr[i-1] > arr[i] {
+				arr[i], arr[i-1] = arr[i-1], arr[i]
+			}
+		}
+	}
 }
