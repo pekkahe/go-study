03bea75755b2b8fa51aca2573b9e1098dd610b7d
pekkahe <pekka.hellsten@gmail.com>
2019-06-23 21:41:37 +0300

    Index improvement to BinarySearch

--- search/search.go
+++ search/search.go
@@ -1,20 +1,16 @@
 package search
 
 func BinarySearch(arr []int, value int) int {
-	if len(arr) > 0 && (value < arr[0] || value > arr[len(arr)-1]) {
-		return -1
-	}
-
 	lo := 0
 	hi := len(arr)
 
 	for lo < hi {
-		i := ((hi - lo) / 2) + lo
+		i := (hi + lo) / 2
 		v := arr[i]
 
 		switch {
 		case value > v:
-			lo = i
+			lo = i + 1
 		case value < v:
 			hi = i
 		default: // value == v
