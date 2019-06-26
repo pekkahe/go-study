307c51110d70f6e9c0d31660f9fdb1af687eae26
Pekka Hellsten <pekka.hellsten@ubisoft.com>
2019-06-26 12:05:44 +0300

    Minor refactoring

--- mongo/main.go
+++ mongo/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -42,77 +43,33 @@ func main() {
 }
 
 func (h PeopleHandler) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-	okResponse := func(v interface{}) {
-		w.WriteHeader(http.StatusOK)
-		e := json.NewEncoder(w)
-		_ = e.Encode(v)
-	}
-	errorResponse := func(v interface{}) {
-		w.WriteHeader(http.StatusInternalServerError)
-		e := json.NewEncoder(w)
-		_ = e.Encode(v)
-	}
-
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
 	switch request.Method {
 	case "GET":
-		p, err := h.getPeople()
-		if err != nil {
-			errorResponse(err)
-			return
-		}
-
-		b, err := json.Marshal(p)
-		if err != nil {
-			errorResponse(err)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(b)
-
+		h.httpGet(w, request)
 	case "POST":
-		body := make([]byte, request.ContentLength)
-
-		_, err := io.ReadFull(request.Body, body)
-		if err != nil {
-			errorResponse(err)
-			return
-		}
-
-		var p Person
-
-		err = json.Unmarshal(body, &p)
-		if err != nil {
-			errorResponse(err)
-			return
-		}
-
-		err = h.addPerson(p.FirstName, p.LastName)
-		if err != nil {
-			errorResponse(err)
-			return
-		}
-
-		okResponse("New person added.")
-
+		h.httpPost(w, request)
 	case "DELETE":
-		query := request.URL.Query()
-		ids, ok := query["id"]
-		if ok {
-			for _, id := range ids {
-				err := h.removePerson(id)
-				if err != nil {
-					errorResponse(err)
-				}
-			}
-
-			okResponse("Person(s) removed.")
-		} else {
-			errorResponse(errors.New("Unknown query parameter(s)"))
-		}
+		h.httpDelete(w, request)
 	}
+}
+
+func (h PeopleHandler) httpGet(w http.ResponseWriter, request *http.Request) {
+	p, err := h.getPeople()
+	if err != nil {
+		serverError(w, err)
+		return
+	}
+
+	b, err := json.Marshal(p)
+	if err != nil {
+		serverError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(b)
 }
 
 func (h PeopleHandler) getPeople() (people []Person, err error) {
@@ -139,16 +96,63 @@ func (h PeopleHandler) getPeople() (people []Person, err error) {
 	return
 }
 
-func (h PeopleHandler) addPerson(firstname, lastname string) (err error) {
+func (h PeopleHandler) httpPost(w http.ResponseWriter, request *http.Request) {
+	body := make([]byte, request.ContentLength)
+	_, err := io.ReadFull(request.Body, body)
+	if err != nil {
+		badRequest(w, err)
+		return
+	}
+
+	var p Person
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		badRequest(w, err)
+		return
+	}
+
+	err = h.addPerson(&p)
+	if err != nil {
+		serverError(w, err)
+		return
+	}
+
+	okResponse(w, "New person added.")
+}
+
+func (h PeopleHandler) addPerson(p *Person) (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	if len(p.FirstName) == 0 || len(p.LastName) == 0 {
+		err = errors.New("Missing first or last name.")
+		return
+	}
+
 	_, err = h.people.InsertOne(ctx, bson.M{
-		"firstname": firstname,
-		"lastname":  lastname,
+		"firstname": p.FirstName,
+		"lastname":  p.LastName,
 	})
 
 	return
+}
+
+func (h PeopleHandler) httpDelete(w http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	ids, ok := query["id"]
+	if !ok {
+		badRequest(w, errors.New("Missing 'id' query parameter(s)."))
+		return
+	}
+
+	for _, id := range ids {
+		err := h.removePerson(id)
+		if err != nil {
+			serverError(w, err)
+		}
+	}
+
+	okResponse(w, "Person(s) removed.")
 }
 
 func (h PeopleHandler) removePerson(id string) (err error) {
@@ -159,4 +163,23 @@ func (h PeopleHandler) removePerson(id string) (err error) {
 	_, err = h.people.DeleteOne(ctx, bson.M{"_id": oid})
 
 	return
+}
+
+func okResponse(w http.ResponseWriter, v interface{}) {
+	w.WriteHeader(http.StatusOK)
+
+	e := json.NewEncoder(w)
+	_ = e.Encode(v)
+}
+func serverError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+
+	e := json.NewEncoder(w)
+	_ = e.Encode(fmt.Sprintf("Error: %v", err))
+}
+func badRequest(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusBadRequest)
+
+	e := json.NewEncoder(w)
+	_ = e.Encode(fmt.Sprintf("Error: %v", err))
 }
